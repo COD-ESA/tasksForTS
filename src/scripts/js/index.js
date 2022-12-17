456bef6faf71560a3ee0990bffffb176db1f1bb6
Sirius100 <esaul-00@mail.ru>
2022-12-17 14:36:08 +0300

    разобрал 1-ю задачу

--- src/scripts/js/index.js
+++ src/scripts/js/index.js
@@ -1,8 +1,25 @@
 "use strict";
+// Задача на подсчет количества HTML элементов
+const { log } = console;
+// возвращает элемент или null если его нет в document
 const testLoadScript = (item) => {
-    const h = document.querySelector(item);
-    if (h instanceof HTMLElement) {
-        h.textContent = 'hello!';
+    const elem = document.querySelector(item);
+    if (elem instanceof HTMLElement)
+        return elem;
+    return null;
+};
+const body = testLoadScript('body');
+const countIncludeChildrens = (element) => {
+    if (element instanceof HTMLElement) {
+        const elementArray = Array.from(element.children);
+        log(`Element ${element.tagName} includes ${element.children.length} items`);
+        log('\n');
+        elementArray.forEach((item) => {
+            if (item instanceof HTMLElement)
+                countIncludeChildrens(item);
+        });
     }
 };
-testLoadScript('h1');
+if (body instanceof HTMLElement) {
+    countIncludeChildrens(body);
+}
--- src/scripts/ts/index.ts
+++ src/scripts/ts/index.ts
@@ -1,8 +1,26 @@
-const testLoadScript = (item:string) => {
-  const h = document.querySelector(item);
-  if (h instanceof HTMLElement) {
-    h.textContent = 'hello!';
+// Задача на подсчет количества HTML элементов
+const { log } = console;
+
+// возвращает элемент или null если его нет в document
+const testLoadScript = (item: string): HTMLElement | null => {
+  const elem = document.querySelector(item);
+  if (elem instanceof HTMLElement) return elem;
+  return null;
+};
+
+const body: HTMLElement | null = testLoadScript('body');
+
+const countIncludeChildrens = (element: HTMLElement) => {
+  if (element instanceof HTMLElement) {
+    const elementArray = Array.from(element.children);
+    log(`Element ${element.tagName} includes ${element.children.length} items`);
+    log('\n');
+    elementArray.forEach((item) => {
+      if (item instanceof HTMLElement) countIncludeChildrens(item);
+    });
   }
 };
 
-testLoadScript('h1');
+if (body instanceof HTMLElement) {
+  countIncludeChildrens(body);
+}
