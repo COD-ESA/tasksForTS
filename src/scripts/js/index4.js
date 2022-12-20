fafed973d13320894de2e470994e95722ae7f229
Sirius100 <esaul-00@mail.ru>
2022-12-20 19:16:45 +0300

    разобрал массив на вложения по ключу

--- src/scripts/js/index4.js
+++ src/scripts/js/index4.js
@@ -2,9 +2,9 @@
 (function _() {
     const { log } = console;
     const array = [
-        { label: 'label1', id: 1, des: 'here des1' },
-        { label: 'label2', id: 2, des: 'here des2' },
-        { label: 'label3', id: 3, des: 'here des3' },
+        { label: 'label1', id: 1, des: 'here des_1' },
+        { label: 'label2', id: 2, des: 'here des_2' },
+        { label: 'label3', id: 3, des: 'here des_3' },
     ];
     const parser = (arr, key) => 
     /* eslint-disable-next-line */
