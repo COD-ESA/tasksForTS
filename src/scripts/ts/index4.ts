eb2565b07bd4f55f2b97ab79ecee3d2fc5e8b4ed
Sirius100 <esaul-00@mail.ru>
2022-12-20 19:04:14 +0300

    разобрал массив на вложения по ключу

--- src/scripts/ts/index4.ts
+++ src/scripts/ts/index4.ts
@@ -6,9 +6,9 @@
     des: string;
   };
   const array: Array<ArrayHash> = [
-    { label: 'label1', id: 1, des: 'here des1' },
-    { label: 'label2', id: 2, des: 'here des2' },
-    { label: 'label3', id: 3, des: 'here des3' },
+    { label: 'label1', id: 1, des: 'here des_1' },
+    { label: 'label2', id: 2, des: 'here des_2' },
+    { label: 'label3', id: 3, des: 'here des_3' },
   ];
   const parser = <T>(arr: Array<T>, key: keyof T) =>
     /* eslint-disable-next-line */
